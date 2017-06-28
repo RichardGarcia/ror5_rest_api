ffcd7e63e13cbd61e6c2a3396e11a83d10cdfdbb
Richard Garcia <richardgarciaph@gmail.com>
2017-06-29 00:42:24 +1200

    add fake data

--- myarticles/app/models/article.rb
+++ myarticles/app/models/article.rb
@@ -1,4 +1,10 @@
 class Article < ApplicationRecord
-  validates :title, presence :true
-  validates :body, presence :true
+  validates :title, presence: true
+  validates :body, presence: true
 end
+
+
+# class Article < ApplicationRecord
+#   validates :title, presence: true
+#   validates :body, presence: true
+# end
