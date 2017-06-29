f9d00c38aa0394b84399849ea3c169d26b95e21b
Richard Garcia <richardgarciaph@gmail.com>
2017-06-29 12:06:02 +1200

    add show single article function

--- myarticles/app/controllers/api/v1/articles_controller.rb
+++ myarticles/app/controllers/api/v1/articles_controller.rb
@@ -1,10 +1,16 @@
 module Api
-  module v1
-    class ArticleController < ApplicationController
+  module V1
+    class ArticlesController < ApplicationController
 
+      # show all articles sort by "created_at" in descending order
       def index
         articles = Article.order('created_at DESC');
-        render json: {status: 'SUCCESS', message: 'Articles loaded', data:articles}, status: ok
+        render json: {status: 'SUCCESS', message: 'Articles loaded', data: articles}, status: :ok
+      end
+      # show single article
+      def show
+        article = Article.find(params[:id]);
+        render json: {status: 'SUCCESS', message: 'Article loaded', data: article}, status: :ok
       end
 
     end
